ff8fe813306da3444a44b54dfbde67f60ed0ad6c
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-12-14 00:37:28 +0700

    chore : Description typing format updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
 				404
 			</h1>
 			<p className="text-sand mt-8 text-[17px] text-center lg:text-xl lg:leading-relaxed lg:mt-10 lg:px-44 font-light">
-				Ooops, you've lost in space!
+				Ooops, you&apos;ve lost in space!
 			</p>
 		</section>
 	);

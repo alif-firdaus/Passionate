17d31329017d650aea3d7b3196533bcc037b5630
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-12-13 10:58:23 +0700

    fix : Connect Page updated

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -1,3 +1,16 @@
+// Import Components //
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import ContactForm from "@/components/contact-form";
+
 export default function Connect() {
-	return <div className="bg-red-500">Connect page</div>;
+	return (
+		<section>
+			<Navbar />
+			<ContactForm />
+			<div className="lg:px-7 px-0 lg:pb-7">
+				<Footer />
+			</div>
+		</section>
+	);
 }

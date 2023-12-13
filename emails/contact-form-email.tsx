e62cf49654d44fbdcc854c7fa94edb46955ba2ff
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-12-13 10:58:54 +0700

    feat : Contact Form Email Template added

---
+++ emails/contact-form-email.tsx
@@ -0,0 +1,22 @@
+interface ContactFormEmailProps {
+	name: string;
+	email: string;
+	message: string;
+}
+
+const ContactFormEmail: React.FC<Readonly<ContactFormEmailProps>> = ({
+	name,
+	email,
+	message,
+}) => (
+	<div>
+		<h1>Someone wants to connect! 🚀</h1>
+		<p>
+			From <strong>{name}</strong> at {email}
+		</p>
+		<h2>Message:</h2>
+		<p>{message}</p>
+	</div>
+);
+
+export default ContactFormEmail;
